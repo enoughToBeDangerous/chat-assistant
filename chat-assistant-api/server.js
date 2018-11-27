dfbbb8d0bc6d4c4e831a51f3d82662b71c9ddcc0
clay <clay@kenobi.attlocal.net>
2018-11-26 20:53:26 -0600

    make git ingore any node_modules folder or .env file

--- chat-assistant-api/server.js
+++
@@ -1,9 +0,0 @@
-const assistant = require('./routes/assistant'); 
-const express = require('express');
-const api_server = express();
-const port = process.env.port || 3001;
-const body_parser = require('body-parser');
-
-api_server.use('/api/assistant', assistant);
-api_server.use(body_parser.json());
-api_server.listen(port, () => {console.log(`listening on port ${port}`)});
